51d02b63783830865e0ef05965099bfe4fda001d
Jwiwiwiii <kbh81293@gmail.com>
2025-01-13 13:56:44 +0530

    Update Moin.py

--- Moin.py
+++ Moin.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7313748542:AAGXu8ezLfSHJdO187aRn_tdpMyQZl8oOW4")
+BOT_TOKEN=("7631118541:AAG8ZHzXMA_JVRVn_WZ9Zj9aMib7vH5Pkcg")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@Itz_sonu_9")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6137914349")
+ADMIN_IDS=("5496190244")
 
 
 
