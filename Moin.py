91e4b20caad62b3049835a3d87c03002216ab019
Jwiwiwiii <kbh81293@gmail.com>
2025-01-13 13:50:59 +0530

    Add files via upload

---
+++ Moin.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7313748542:AAGXu8ezLfSHJdO187aRn_tdpMyQZl8oOW4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Itz_sonu_9")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6137914349")
+
+
+
+
+
+  
